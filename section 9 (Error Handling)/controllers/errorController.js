630c991d18636757f88534f5e6987211e1db9e34
Ahmed Atef <66911947+Ahmed3tef@users.noreply.github.com>
2021-12-21 11:15:59 +0200

    Add files via upload

---
+++ section 9 (Error Handling)/controllers/errorController.js
@@ -0,0 +1,65 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
+  const message = `Duplicate field value: ${value}. please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(err => err.message);
+  const message = `Invalid input data: ${errors.join(', ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // 1) operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // programming, unknown error: don't leak err details.
+  } else {
+    //1- log it
+    console.log('Error 💥' + err);
+
+    // 2) send message to client
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
+  }
+};
